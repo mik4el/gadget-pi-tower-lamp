9fb3126476078c2da6e4d1bbfecfc81c50e3aeef
Mikael Andersson <mik4el.4ndersson@gmail.com>
2014-01-21 22:17:22 +0100

    added function RGBForWindow and tests

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-def RGBForPixel(pixelsInImage, x, y):
+def RGBForPixel(x, y, pixelsInImage):
     return pixelsInImage[x, y]
+
+
+def RGBForWindow(windowNumber, pixelsInImage):
+    POSITION_FOR_WINDOW_NUMBER = {
+        0: [440, 299],
+        1: [440, 277],
+        2: [440, 255],
+        3: [440, 234],
+        4: [440, 212],
+        5: [441, 189],
+        6: [441, 169],
+        7: [441, 148],
+        8: [441, 126],
+        9: [440, 105],
+    }
+    x = POSITION_FOR_WINDOW_NUMBER[windowNumber][0]
+    y = POSITION_FOR_WINDOW_NUMBER[windowNumber][1]
+    return RGBForPixel(x, y, pixelsInImage)
--- tests.py
+++ tests.py
@@ -20,10 +20,31 @@ class TestRGBForPixel(unittest.TestCase):
         # Check that a array of length 3 is returned for a pixel
         self.assertEqual(len(self.pixelsInImage[1, 1]), 3)
 
+    def test_RGBForWindow(self):
+        """
+        Test function RGBForWindow that maps window index to pixel position and then gets RGB for that position
+        """
+        # Know values per window for tower_test_01
+        TOWER_TEST_IMAGE_01_RGB_PER_WINDOW = [
+            (116, 32, 231),
+            (77, 43, 218),
+            (116, 136, 235),
+            (108, 115, 221),
+            (169, 231, 255),
+            (47, 117, 212),
+            (54, 98, 203),
+            (82, 78, 214),
+            (101, 45, 178),
+            (223, 96, 255)
+        ]
+        self.assertEqual(len(TOWER_TEST_IMAGE_01_RGB_PER_WINDOW), 10)
+        for i in range(len(TOWER_TEST_IMAGE_01_RGB_PER_WINDOW)):
+            self.assertEqual(main.RGBForWindow(i, pixelsInImage=self.pixelsInImage), TOWER_TEST_IMAGE_01_RGB_PER_WINDOW[i])
+
     def test_RGBForPixel(self):
         """
         Test function RGBForPixel
         """
-        result = main.RGBForPixel(pixelsInImage=self.pixelsInImage, x=1, y=1)
+        result = main.RGBForPixel(x=1, y=1, pixelsInImage=self.pixelsInImage)
         self.assertEqual(len(result), 3)
         self.assertEqual(self.pixelsInImage[1, 1], result)
