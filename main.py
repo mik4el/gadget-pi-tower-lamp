2d6d975ffa2ea7a93ea9f62e564cef2b8d184484
Mikael Andersson <mik4el.4ndersson@gmail.com>
2014-01-25 18:25:18 +0100

    added animation from color to color to lamp that triggers when tower has changed

--- main.py
+++ main.py
@@ -54,10 +54,15 @@ def hexFromRGB(RGB):
 
 
 class PILampModel:
-    def __init__(self, rgb, isOn):
-        self.rgb = rgb
+    def __init__(self, r, g, b, isOn):
+        self.r = r
+        self.g = g
+        self.b = b
         self.isOn = isOn
         print "Lamp model created!"
+
+    def getRGB(self):
+        return self.r, self.g, self.b
 
 
 class PITowerModel:
@@ -69,6 +74,7 @@ class PITowerModel:
 
 
 class PITowerController(threading.Thread):
+
     def __init__(self, imageName, towerControllerQueue, lampControllerQueue):
         threading.Thread.__init__(self)
         self.imageName = imageName
@@ -76,10 +82,70 @@ class PITowerController(threading.Thread):
         self.towerControllerQueue = towerControllerQueue
         self.lampControllerQueue = lampControllerQueue
         self.currentTowerModel = None
-        self.currentLampModel = None
+        self.oldTowerModel = None
+        self.currentLampModel = PILampModel(255, 255, 255, True)
         self.ticks = 0
         self.tickTowerUpdate = 0
         self.tickLampUpdate = 0
+        self.updateHZ = 0.1
+        self.deltaR = 0.0
+        self.deltaG = 0.0
+        self.deltaB = 0.0
+        self.lampIsAnimating = False
+
+    def startLampAnimation(self):
+        print "Start lamp animation"
+        startRGB = self.currentLampModel.getRGB()
+        endRGB = self.currentTowerModel.averageWindowRGB
+        steps = 1/self.updateHZ
+
+        #Calculate deltas for RGB channels
+        self.deltaR = float((endRGB[0]-startRGB[0]))/float(steps)
+        self.deltaG = float((endRGB[1]-startRGB[1]))/float(steps)
+        self.deltaB = float((endRGB[2]-startRGB[2]))/float(steps)
+
+        #Start animation
+        self.lampIsAnimating = True
+
+    def checkLampAnimationReady(self):
+        readyRGB = self.currentTowerModel.averageWindowRGB
+
+        #Check if self.currentLampModel matches with readyRGB, set deltas to zero if match
+        diffR = readyRGB[0] - self.currentLampModel.r
+        if diffR >= 1 or diffR >= 1:
+            self.deltaR = 0.0
+            self.currentLampModel.r = readyRGB[0]
+        diffG = readyRGB[1] - self.currentLampModel.g
+        if diffG >= 1 or diffG >= 1:
+            self.deltaG = 0.0
+            self.currentLampModel.g = readyRGB[1]
+        diffB = readyRGB[2] - self.currentLampModel.b
+        if diffB >= 1 or diffB >= 1:
+            self.deltaB = 0.0
+            self.currentLampModel.b = readyRGB[2]
+
+        #If all deltas == 0.0 then animation is over
+        if self.deltaR == 0.0 and self.deltaG == 0.0 and self.deltaB == 0.0:
+            self.lampIsAnimating = False
+
+    def updateLamp(self):
+        if self.lampIsAnimating:
+            # Add deltas to lamp
+            self.currentLampModel.r += self.deltaR
+            self.currentLampModel.g += self.deltaG
+            self.currentLampModel.b += self.deltaB
+
+            # Check if ready
+            self.checkLampAnimationReady()
+
+            # Add new lamp to controller queue
+            self.lampControllerQueue.put(self.currentLampModel)
+
+    def towerModelChanged(self, newTowerModel):
+        self.oldTowerModel = self.currentTowerModel
+        self.currentTowerModel = newTowerModel
+        self.towerControllerQueue.put(self.currentTowerModel)
+        self.startLampAnimation()
 
     def downloadTowerImage(self):
         #os.system("curl -o tower_temp.jpg http://89.253.86.245//axis-cgi/jpg/image.cgi?resolution=800x450")
@@ -102,31 +168,24 @@ class PITowerController(threading.Thread):
         allWindowsColor = RGBForAllWindows(pixelsInImage)
         averageColor = averageRGB(allWindowsColor)
 
-        # Create PITowerModel and put in towerControllerQueue
+        # Create PITowerModel and mark as changed
         towerModel = PITowerModel(allWindowsColor, averageColor, self.image)
-        self.towerControllerQueue.put(towerModel)
-        self.currentTowerModel = towerModel
 
-    def updateLamp(self):
-        # Create PILampModel and put in lampControllerQueue
-        if self.ticks % 3 == 0:
-            lampModel = PILampModel(self.currentTowerModel.averageWindowRGB, False)
-            self.lampControllerQueue.put(lampModel)
-            self.currentLampModel = lampModel
+        # Check if PITowerModel changed
+        if self.currentTowerModel:
+            if towerModel.averageWindowRGB != self.currentTowerModel.averageWindowRGB:
+                self.towerModelChanged(towerModel)
         else:
-            lampModel = PILampModel(self.currentTowerModel.averageWindowRGB, True)
-            self.lampControllerQueue.put(lampModel)
-            self.currentLampModel = lampModel
+            self.currentTowerModel = towerModel
+            self.towerModelChanged(towerModel)
 
     def tick(self):
-        if self.ticks == self.tickTowerUpdate + 100 or self.ticks == 0:
+        if self.ticks == self.tickTowerUpdate + 5/self.updateHZ or self.ticks == 0:
             self.tickTowerUpdate = self.ticks
             self.updateTower()
-        if self.ticks == self.tickLampUpdate + 10 or self.ticks == 0:
-            self.tickLampUpdate = self.ticks
-            self.updateLamp()
+        self.updateLamp()  # lamp should update as often as possible
         self.ticks += 1
-        time.sleep(0.1)
+        time.sleep(self.updateHZ)
         print self.ticks
 
     def run(self):
@@ -165,7 +224,7 @@ class PITowerLampVisualization:
         # Draw lampModel
         if self.lampModel:
             if self.lampModel.isOn:
-                self.canvas.create_rectangle(250, 550, 450, 650, fill=hexFromRGB(self.lampModel.rgb))
+                self.canvas.create_rectangle(250, 550, 450, 650, fill=hexFromRGB(self.lampModel.getRGB()))
             else:
                 self.canvas.create_rectangle(250, 550, 450, 650, fill="#000000")
 
